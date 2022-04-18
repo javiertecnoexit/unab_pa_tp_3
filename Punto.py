919519682de05cd21f7dd91e0d3b1cf54e6fa43e
tecnoexit <tecnoexit@gmail.com>
2022-04-18 15:13:50 -0300

    .

---
+++ Punto.py
@@ -0,0 +1,11 @@
+from re import X
+
+
+class Punto():
+    def __init__(x,y):
+        self.x=X
+        self.y=y
+
+    def Impresion():
+        print("el punto se encuentra en la coordenada x={0} coordenada y={1}", str(self.x), str(self.y))
+
